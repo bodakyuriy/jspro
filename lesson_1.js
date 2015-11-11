8771cf8cca379c1fe7204dfd725d359733218120
bodakyuriy <bodakyura@gmail.com>
2015-11-11 17:46:57 +0200

    add h/w

---
+++ lesson_1.js
@@ -0,0 +1,21 @@
+var Number = 10;
+var String = "Hello, World";
+var Boolean = true;
+var Null = null;
+var Undefined = undefined;
+var Object = { name: "Þðà" };
+console.log(Number + '\n', String + '\n', Boolean + '\n', Null + '\n', Undefined + '\n', Object.name)
+
+var a = +"123";
+console.log(typeof (a))
+
+var b = 123;
+var b = b + "abc";
+console.log(typeof (b));
+
+var c = "123";
+var d = 123;
+var e = c === d;
+console.log(typeof (e));
+
+
